f417c2ebd47222549cae044914685cd54643133c
varshithsolleti2007 <varshithsolleti@gmail.com>
2026-07-12 05:58:16 +0530

    feat: configure API rewrites in next.config.ts to proxy requests to backend service

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*',
+        destination: 'https://darshanease-backend.onrender.com/api/:path*',
       },
     ];
   },
